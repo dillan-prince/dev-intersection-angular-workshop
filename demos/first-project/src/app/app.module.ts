8289a822a91b3b4047f3631d0ea806fd07324e56
Dillan Prince <dillanprince@gmail.com>
2018-12-02 13:55:29 -0600

    header'

--- demos/first-project/src/app/app.module.ts
+++ demos/first-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderDComponent } from './header-d/header-d.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderDComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
