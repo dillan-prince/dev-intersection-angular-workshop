a8f5e7055aa16116e109a1f67c82da16a335ecaa
Dillan Prince <dillanprince@gmail.com>
2018-12-03 13:26:24 -0600

    import shared library from node_modules path

--- demos/shared-library/src/app/app.module.ts
+++ demos/shared-library/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SharedLibModule } from 'shared-lib';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedLibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
